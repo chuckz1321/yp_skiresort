7c2146617f34dbf09caf261a2f18672fc37b29d6
zcy960312 <zcy960312@gmail.com>
2019-02-20 20:45:07 -0500

    config jdbc mybatis db2

--- src/main/java/com/example/yp_skiresort/Controller/TestController.java
+++ src/main/java/com/example/yp_skiresort/Controller/TestController.java
@@ -1,16 +1,24 @@
 package com.example.yp_skiresort.Controller;
 
 import com.example.yp_skiresort.Entity.ResponseMessage;
+import com.example.yp_skiresort.Entity.TUser;
+import com.example.yp_skiresort.service.IUserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    private IUserService userService;
 
     @ApiOperation(value="welcome", notes="welcome by url id")
     @ApiImplicitParam(name = "id", value = "ID", required = true, dataType = "Long",paramType = "path")
@@ -21,4 +29,15 @@ public class TestController {
         rm.setResponseBody("welcome " + id + "to ski test");
         return rm;
     }
+
+    @ApiOperation(value="Test", notes="dbtest")
+    @RequestMapping(value="/getAllUser", method= RequestMethod.GET)
+    public ResponseMessage welcome() {
+        ResponseMessage<List<TUser>> rm = new ResponseMessage<>();
+        rm.setHttpCode("200");
+        rm.setResponseBody(userService.getAllUser());
+        return rm;
+    }
+
+
 }
