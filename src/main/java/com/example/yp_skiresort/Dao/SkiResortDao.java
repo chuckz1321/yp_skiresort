180d85bf4b5ecb47a1d5bfff243fb46c0d943a38
Wang LYU <wlyu2@uwo.ca>
2019-02-22 10:53:16 -0500

    switch the type of this class from abstract class to interface

--- src/main/java/com/example/yp_skiresort/Dao/SkiResortDao.java
+++ src/main/java/com/example/yp_skiresort/Dao/SkiResortDao.java
@@ -2,7 +2,7 @@ package com.example.yp_skiresort.Dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public abstract class SkiResortDao {
+public interface SkiResortDao {
     public abstract void getResortListByCountry(String country);
     public abstract void getResortListByPriceRange(int max, int min);
     public abstract void getResortListBySlopeRating(int rating);
