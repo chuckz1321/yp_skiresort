fde2fcaf14ee1958c580b3bfa0b038b11b1dbc09
Wang LYU <wlyu2@uwo.ca>
2019-02-21 23:27:10 -0500

    new entity class for skiresort

---
+++ src/main/java/com/example/yp_skiresort/Entity/SkiResort.java
@@ -0,0 +1,15 @@
+package com.example.yp_skiresort.Entity;
+
+public class SkiResort {
+    private String resortName;
+    private String continent;
+    private String country;
+    private String province;
+    private int altitude;
+    private int beginnerSlopeDistance;
+    private int intermediateSlopeDistance;
+    private int difficultSlopeDistance;
+    private int adultTicketPrice;
+    private int youthTicketPrice;
+    private int childTicketPrice;
+}
