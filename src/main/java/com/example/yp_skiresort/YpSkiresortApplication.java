281fe7ed0588a1d258299414ce782ac3b909cabe
Wang LYU <wlyu2@uwo.ca>
2019-03-09 11:10:18 -0500

    add mapperscan annotation

--- src/main/java/com/example/yp_skiresort/YpSkiresortApplication.java
+++ src/main/java/com/example/yp_skiresort/YpSkiresortApplication.java
@@ -1,5 +1,6 @@
 package com.example.yp_skiresort;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
+@MapperScan("com.example.yp_skiresort.Dao")
 public class YpSkiresortApplication {
 
     public static void main(String[] args) {
