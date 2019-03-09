ed9ff54f5e28e3ae4c4ee070c8876e6b7bcd5428
Wang LYU <wlyu2@uwo.ca>
2019-03-09 11:18:45 -0500

    service implementation class

---
+++ src/main/java/com/example/yp_skiresort/service/impl/SkiResortSvcImpl.java
@@ -0,0 +1,33 @@
+package com.example.yp_skiresort.service.impl;
+
+import com.example.yp_skiresort.Dao.SkiResortDao;
+import com.example.yp_skiresort.Entity.SkiResort;
+import com.example.yp_skiresort.service.SkiResortSvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkiResortSvcImpl implements SkiResortSvc {
+    @Autowired
+    SkiResortDao resortDao;
+    public List<SkiResort> getSkiResortListByCountry(String country)
+    {
+        List<SkiResort> resorts = resortDao.getSkiResortListByCountry(country);
+        return resorts;
+    }
+    public List<SkiResort> getResortListByPriceRange(int max, int min)
+    {
+        List<SkiResort> resorts = resortDao.getResortListByPriceRange(max,min);
+        return resorts;
+    }
+    public List<SkiResort> getResortListBySlopeRating(int rating){
+        List<SkiResort> resorts = resortDao.getResortListBySlopeRating(rating);
+        return resorts;
+    }
+    public List<SkiResort> getResortListByPartialName(String resortName){
+        List<SkiResort> resorts = resortDao.getResortListByPartialName(resortName);
+        return resorts;
+    }
+}
