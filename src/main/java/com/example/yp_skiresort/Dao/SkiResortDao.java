1f16d04a0136246c8e8f8dd1a32f59f7ecbeb43b
Wang LYU <wlyu2@uwo.ca>
2019-03-09 11:09:54 -0500

    add mapper annotation to enable mapper scan on this class

--- src/main/java/com/example/yp_skiresort/Dao/SkiResortDao.java
+++ src/main/java/com/example/yp_skiresort/Dao/SkiResortDao.java
@@ -1,12 +1,14 @@
 package com.example.yp_skiresort.Dao;
 import java.util.*;
 import com.example.yp_skiresort.Entity.SkiResort;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Mapper
 public interface SkiResortDao {
-    public abstract List<SkiResort> getResortListByCountry(String country);
+    public abstract List<SkiResort> getSkiResortListByCountry(String country);
     public abstract List<SkiResort> getResortListByPriceRange(@Param("max") int max, @Param("min") int min);
     public abstract List<SkiResort> getResortListBySlopeRating(int rating);
     public abstract List<SkiResort> getResortListByPartialName(String resortName);
