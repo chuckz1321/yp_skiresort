bba00d9304aa81e511950d9a551cbc46765b3023
Wang LYU <wlyu2@uwo.ca>
2019-03-10 20:38:35 -0400

    change the type of the input parameter

--- src/main/java/com/example/yp_skiresort/service/SkiResortSvc.java
+++ src/main/java/com/example/yp_skiresort/service/SkiResortSvc.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface SkiResortSvc {
     public abstract List<SkiResort> getSkiResortListByCountry(String country);
     public abstract List<SkiResort> getResortListByPriceRange(int max, int min);
-    public abstract List<SkiResort> getResortListBySlopeRating(int rating);
+    public abstract List<SkiResort> getResortListBySlopeRating(float rating);
     public abstract List<SkiResort> getResortListByPartialName(String resortName);
 }
