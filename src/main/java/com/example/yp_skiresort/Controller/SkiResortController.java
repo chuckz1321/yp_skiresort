ee6bd5bc2206fdeb06d193712ffdd2de62b80a0a
Wang LYU <wlyu2@uwo.ca>
2019-03-13 00:26:59 -0400

    enable search with multiple conditions

--- src/main/java/com/example/yp_skiresort/Controller/SkiResortController.java
+++ src/main/java/com/example/yp_skiresort/Controller/SkiResortController.java
@@ -20,9 +20,10 @@ public class SkiResortController {
     @RequestMapping(value="/test",method=RequestMethod.GET)
     @ResponseBody
     public ResponseMessage<List<SkiResort>> test(@RequestParam("country") String country){
-        svc.getResortListByPartialName("aarjaeng");
-        svc.getResortListByPriceRange(100,0);
-        svc.getResortListBySlopeRating(Float.parseFloat("2.0"));
+        //svc.getResortListByPartialName("aarjaeng");
+        //svc.getResortListByPriceRange(100,0);
+        //svc.getResortListBySlopeRating(Float.parseFloat("2.0"));
+        svc.getResortListByMultipleConditions("aarjaeng",2000, 0,2,"Canada");
         ResponseMessage message = new ResponseMessage();
         message.setResponseBody(svc.getSkiResortListByCountry(country));
         message.setHttpCode("200");
@@ -34,8 +35,8 @@ public class SkiResortController {
         String query = inputQuery.getQuery();
         List<SkiResort> resorts = new ArrayList<SkiResort>();
         String[] subqueries = query.split("&");
-        for(String subquery: subqueries){
-            String[] queryParts = subquery.split("=");
+        if(subqueries.length == 1){
+            String[] queryParts = subqueries[0].split("=");
             switch(queryParts[0]){
                 case "country":
                     if( !queryParts[1].equals("") ) {
@@ -55,13 +56,50 @@ public class SkiResortController {
                         resorts = svc.getResortListByPartialName(queryParts[1]);
                     }
                     break;
-                case "sloperate":
+                case "sloperating":
                     if (!queryParts[1].equals("")){
                         resorts = svc.getResortListBySlopeRating(Integer.parseInt(queryParts[1]));
                     }
                     break;
             }
         }
+        else{
+            String country = "";
+            int max = -1;
+            int min = -1;
+            String resortName = "";
+            float rating = 0;
+            for(String subquery:subqueries){
+                String[] queryParts = subquery.split("=");
+                switch(queryParts[0]){
+                    case "country":
+                        if( !queryParts[1].equals("") ) {
+                            country = queryParts[1];
+                        }
+                        break;
+                    case "price":
+                        if( !queryParts[1].equals("") ) {
+                            String[] range_end = queryParts[1].split("=");
+                            max = Integer.parseInt(range_end[1]);
+                            min = Integer.parseInt(range_end[0]);
+                        }
+                        break;
+                    case "resortname":
+                        if( !queryParts[1].equals("") ){
+                            resortName = queryParts[1];
+                        }
+                        break;
+                    case "sloperating":
+                        if( !queryParts[1].equals("") ){
+                            rating = Float.parseFloat(queryParts[1]);
+                        }
+                        break;
+
+                }
+            }
+            resorts = svc.getResortListByMultipleConditions(resortName,max,min,rating,country);
+        }
+
 
         ResponseMessage<List<SkiResort>> message = new ResponseMessage<List<SkiResort>>();
         message.setResponseBody(resorts);
