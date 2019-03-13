5c26dd2449b4cba016647b7b0e30b8354f9db645
zcy960312 <zcy960312@gmail.com>
2019-03-12 21:44:25 -0400

    change some db problem

--- src/main/java/com/example/yp_skiresort/Controller/TestController.java
+++ src/main/java/com/example/yp_skiresort/Controller/TestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/unavi")
 public class TestController {
 
     @Autowired
